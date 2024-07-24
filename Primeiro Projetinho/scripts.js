5a1e6c5d3f8066f429b8a55ef9aaf8200764e5e3
Isaac-git-jonats <issacabreu@gmail.com>
2024-07-24 17:08:04 -0300

    Create scripts.js

---
+++ Primeiro Projetinho/scripts.js
@@ -0,0 +1,9 @@
+let formulario = document.querySelector(".formulario")
+
+function ativarformulario(){
+    formulario.style.left = "32.40%"
+}
+
+function destivarformulario(){
+    formulario.style.left = "-50%"
+}
