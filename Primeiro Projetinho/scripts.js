e25829a0b1bf6c1706cfc8cdb568c60d3813875e
Isaac Jonatas <issacabreu@gmail.com>
2024-09-12 15:18:50 -0300

    Delete Primeiro Projetinho directory

--- Primeiro Projetinho/scripts.js
+++
@@ -1,9 +0,0 @@
-let formulario = document.querySelector(".formulario")
-
-function ativarformulario(){
-    formulario.style.left = "32.40%"
-}
-
-function destivarformulario(){
-    formulario.style.left = "-50%"
-}
